d1a1844ebfc406359bb87d054603b60d3580064b
Berk Civelek <berkcvlk@gmail.com>
2021-10-20 21:56:00 +0300

    fix: container fixed height

--- src/containers/TodoContainer/styles.ts
+++ src/containers/TodoContainer/styles.ts
@@ -2,10 +2,9 @@ import { styled } from "stitches.config";
 
 export const Container = styled("div", {
   width: "700px",
-  height: "300px",
 
-  margin: "92px auto 0",
-  padding: "0 50px",
+  margin: "0 auto",
+  padding: "92px 50px",
 });
 
 export const Title = styled("h1", {
