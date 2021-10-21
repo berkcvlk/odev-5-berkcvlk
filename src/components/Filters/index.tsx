b8c6c61b46f34d556643f92bb9bcd83e9bb4f338
Berk Civelek <berkcvlk@gmail.com>
2021-10-21 20:37:33 +0300

    refactor: filters button active prop switch

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -1,22 +1,36 @@
+import { useState } from "react";
+
 import { useTodo } from "hooks";
+import { TodoStatus } from "contexts/Todo/types";
 import * as S from "./styles";
 
 const Filters = () => {
   const { changeFilterToken } = useTodo();
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const filters: { filter: TodoStatus; name: string }[] = [
+    { filter: "ALL", name: "All" },
+    { filter: "ACTIVE", name: "Active" },
+    { filter: "COMPLETED", name: "Completed" },
+  ];
+
+  const onFilterTokenChange = (filter: TodoStatus, index: number) => {
+    changeFilterToken(filter);
+    setActiveIndex(index);
+  };
 
   return (
     <S.Filters>
       <S.See>See</S.See>
       <S.Buttons>
-        <S.FilterButton onClick={() => changeFilterToken("ALL")} isActive>
-          All
-        </S.FilterButton>
-        <S.FilterButton onClick={() => changeFilterToken("ACTIVE")} isActive>
-          Active
-        </S.FilterButton>
-        <S.FilterButton onClick={() => changeFilterToken("COMPLETED")} isActive>
-          Completed
-        </S.FilterButton>
+        {filters.map((item, index) => (
+          <S.FilterButton
+            onClick={() => onFilterTokenChange(item.filter, index)}
+            isActive={activeIndex === index}
+          >
+            {item.name}
+          </S.FilterButton>
+        ))}
       </S.Buttons>
     </S.Filters>
   );
