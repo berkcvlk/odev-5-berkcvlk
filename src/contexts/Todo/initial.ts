a249f0ce63c4b0f5e608783398eac365773fe384
Berk Civelek <berkcvlk@gmail.com>
2021-10-21 13:32:30 +0300

    remove: a gew initial data

--- src/contexts/Todo/initial.ts
+++ src/contexts/Todo/initial.ts
@@ -1,9 +1,5 @@
 export const initialState = {
-  todoList: [
-    { id: 1001, text: "What happened to Monday?", isChecked: true },
-    { id: 1002, text: "Who Am I?", isChecked: false },
-    { id: 1003, text: "Legend", isChecked: false },
-  ],
+  todoList: [{ id: 1001, text: "What happened to Monday?", isChecked: true }],
   addTodo: () => {},
   removeTodo: () => {},
   toggleChecked: () => {},
